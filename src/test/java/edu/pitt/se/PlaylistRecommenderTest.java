5a21f4f08722a74e2a677ed8379f0c0bd2d866dc
David Ejindu <ejindudavid540@gmail.com>
2025-11-20 11:38:18 -0500

    test

--- src/test/java/edu/pitt/se/PlaylistRecommenderTest.java
+++ src/test/java/edu/pitt/se/PlaylistRecommenderTest.java
@@ -1,12 +1,33 @@
 package edu.pitt.se;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class PlaylistRecommenderTest {
 
     @Test
-    public void placeholder() {
-        assertTrue(true);
+    public void testEnergy() {
+        ArrayList<Integer> bpms1 = new ArrayList<>(3);
+        bpms1.add(200);
+        bpms1.add(200);
+        bpms1.add(1);
+        assertEquals("MEDIUM", PlaylistRecommender.classifyEnergy(bpms1));
+
+        ArrayList<Integer> bpms2 = new ArrayList<>(1);
+        bpms2.add(100);
+        assertEquals("MEDIUM", PlaylistRecommender.classifyEnergy(bpms2));
+
+        ArrayList<Integer> bpms3 = new ArrayList<>(2);
+        bpms3.add(100);
+        bpms3.add(300);
+        assertEquals("HIGH", PlaylistRecommender.classifyEnergy(bpms3));
+
+        ArrayList<Integer> bpms4 = new ArrayList<>(2);
+        bpms3.add(100);
+        bpms3.add(21);
+        assertEquals("LOW", PlaylistRecommender.classifyEnergy(bpms3));
+
     }
 }
